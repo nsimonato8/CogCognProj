5d659a41a889c5a65cd073b26f92db0fc5ad2cce
nsimonato8 <nsimonato8@gmail.com>
2022-12-17 19:07:31 +0100

    Dataset import fix continues. Some optimizations are made

--- import_pp/imp.py
+++ import_pp/imp.py
@@ -1,32 +1,16 @@
+"""
+Importing & Preprocessing module
+"""
 import numpy as np
 import torch.utils.data
 import torchvision as tv
-# Importing & Preprocessing
 from matplotlib import pyplot as plt
 from torchvision.transforms import transforms
 
-TRAINING_DIR = "import_pp/tmp/rps/"
-VALIDATION_DIR = "import_pp/tmp/rps-test-set/"
 images_count = 32  # Dummy value
 seed_ = 123
 batch_size_tr, batch_size_vd = images_count, images_count
 img_height, img_width = 150, 150
-
-
-# train_datagen = ImageDataGenerator(rescale=1. / 255)  # Only rescaling is done, in order to not introduce noise in the data. That will be done succesively.
-# test_datagen = ImageDataGenerator(rescale=1. / 255)
-#
-# train_generator = train_datagen.flow_from_directory(
-#     TRAINING_DIR,
-#     target_size=(img_height, img_width),
-#     batch_size=batch_size_tr,
-#     class_mode='categorical')
-#
-# validation_generator = test_datagen.flow_from_directory(
-#     VALIDATION_DIR,
-#     target_size=(img_height, img_width),
-#     batch_size=batch_size_vd,
-#     class_mode='categorical')
 
 
 def get_mean_std(dataset):
