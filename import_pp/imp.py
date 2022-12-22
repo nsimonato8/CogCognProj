e3e565f6af8f3b8d44dc7c83e797b3e16f73c844
nsimonato8 <nsimonato8@gmail.com>
2022-12-22 12:12:38 +0100

    show_processed_imgs fix

--- import_pp/imp.py
+++ import_pp/imp.py
@@ -31,8 +31,8 @@ def show_processed_imgs(dataset) -> None:
     from torch.utils.data.dataloader import default_collate
     loader = torch.utils.data.DataLoader(dataset, batch_size=6, shuffle=True,
                                          collate_fn=lambda x: default_collate(x).cpu().detach())
-    batch = loader.__iter__().__next__()
-    images, labels = batch
+    batch = loader.__iter__()
+    images, labels = batch[0]
 
     grid = tv.utils.make_grid(images, n_row=3)
     plt.figure(figsize=(25, 25))
